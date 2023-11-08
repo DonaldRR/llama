77772eeb017213b0ac5f2ed296ef77834980adc2
DonaldRR <13822743137@163.com>
2023-11-08 05:21:46 +0000

    update download.sh

--- download.sh
+++ download.sh
@@ -4,16 +4,10 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 set -e
-
-read -p "Enter the URL from email: " PRESIGNED_URL
-echo ""
-read -p "Enter the list of models to download without spaces (7B,13B,70B,7B-chat,13B-chat,70B-chat), or press Enter for all: " MODEL_SIZE
-TARGET_FOLDER="."             # where all files should end up
+PRESIGNED_URL="https://download.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoidjJldXVvamJrdXkzOGk0MWc1MWRpczl2IiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQubGxhbWFtZXRhLm5ldFwvKiIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTY5OTUwMzc2OH19fV19&Signature=MEykOlM%7ENVZIfMdloC-bOdffBHTvU35bV38LWqBIkyO-Zy0moZt29qGP48FxRAVOhVI-1lSFTcl6pe1R9sz7LwqhEc%7E%7EGJygxRyVBXgsSsxkDcP8LnxEUhMg3%7EvQ%7EauEH0tPClKsTQdHlciQBxy9ISsvvzoOhWvGBSGnE-YtRd0OCs6uemVuEqdvZVMnyW1K3IRogrHm7RlwrebYX4T-7P4JnqbhGOrA14IfJadVtcKbaDptUOMEQJDRxxvGhU5rz9kLX8NxbVERwn-OSDMbBWYJrPKyGNhVpyWI2Gl46PjL2qqNqnzP%7EsC6T%7Ee27McpvMAeukOOCFRQdNoPA3AV4A__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=639116591749059"
+MODEL_SIZE="7B"
+TARGET_FOLDER="/content/llama/checkpoints"             # where all files should end up
 mkdir -p ${TARGET_FOLDER}
-
-if [[ $MODEL_SIZE == "" ]]; then
-    MODEL_SIZE="7B,13B,70B,7B-chat,13B-chat,70B-chat"
-fi
 
 echo "Downloading LICENSE and Acceptable Usage Policy"
 wget --continue ${PRESIGNED_URL/'*'/"LICENSE"} -O ${TARGET_FOLDER}"/LICENSE"
